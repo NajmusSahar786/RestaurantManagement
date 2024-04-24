5365dbd10fb083a80135e387d89995f32b639973
Najmus Sahar <najmus.sahar77@gmail.com>
2024-04-25 00:44:18 +0530

    Update MenuItem.cs

--- RestaurantManagement/Models/MenuItem.cs
+++ RestaurantManagement/Models/MenuItem.cs
@@ -2,7 +2,7 @@
 {
     public class MenuItem
     {
-        public int menuItemId { get; set; }
+        public int MenuItemId { get; set; }
         public string Title {  get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
