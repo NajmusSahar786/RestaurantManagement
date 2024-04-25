18f54357dc3a865e86c2cd166f7ff7ad0bba13f3
Najmus Sahar <najmus.sahar77@gmail.com>
2024-04-26 00:21:54 +0530

    Model Validation

--- RestaurantManagement/Controllers/HomeController.cs
+++ RestaurantManagement/Controllers/HomeController.cs
@@ -47,14 +47,17 @@ namespace RestaurantManagement.Controllers
         [HttpPost]
         //Model binding maps data in an HTTP request to controller action method parameters
         //The action parameters may be simple types such as integers, strings, etc or complex types like Customer, Employee, Order etc.
-        public RedirectToActionResult Create(Employee2 employee)
+        public IActionResult Create(Employee2 employee)
         {
-            Employee2 newEmployee=_employeeRepository.Add(employee);
-            return RedirectToAction("Details",new { id=newEmployee.Id});
-            //  When an HTTP request arrives at our MVC application it is the Controller action method that handles the incoming request.Let's say we want to view employee details whose ID is 2. For this we issue a GET request to the following URL
-            //http://localhost:48118/home/details/2
-            //The id value 2 in the request URL is mapped to the id parameter of the details(int? id) action method.
-
+            if (ModelState.IsValid)
+            {
+                Employee2 newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("Details", new { id = newEmployee.Id });
+                //  When an HTTP request arrives at our MVC application it is the Controller action method that handles the incoming request.Let's say we want to view employee details whose ID is 2. For this we issue a GET request to the following URL
+                //http://localhost:48118/home/details/2
+                //The id value 2 in the request URL is mapped to the id parameter of the details(int? id) action method.
+            }
+            return View();
         }
     }
 }
--- RestaurantManagement/Models/Employee2.cs
+++ RestaurantManagement/Models/Employee2.cs
@@ -1,9 +1,16 @@
-﻿namespace RestaurantManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManagement.Models
 {
     public class Employee2
     {
         public int Id { get; set; }
+        [Required, MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
         public Dept Department { get; set; }
     }
