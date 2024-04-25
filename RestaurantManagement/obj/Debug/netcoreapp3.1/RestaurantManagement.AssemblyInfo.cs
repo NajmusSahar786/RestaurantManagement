a930ef23194194c102583d7b1b7b7de522bf61ca
Najmus Sahar <najmus.sahar77@gmail.com>
2024-04-25 18:08:04 +0530

    f

--- RestaurantManagement/obj/Debug/netcoreapp3.1/RestaurantManagement.AssemblyInfo.cs
+++ RestaurantManagement/obj/Debug/netcoreapp3.1/RestaurantManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054c8eb6605998c2961de4397659ec895b91a2a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fca2eee6695215199963a8f22b27a7908a85916")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
