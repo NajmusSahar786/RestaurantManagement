0494267323540f36ea7dd69fc8195e2771a7dec3
Najmus Sahar <najmus.sahar77@gmail.com>
2024-05-02 15:36:43 +0530

    Using sql server with entity framework core
    Registering dbcontext service and sql server () in configureservice method of startup class

--- RestaurantManagement/Startup.cs
+++ RestaurantManagement/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -26,6 +27,8 @@ namespace RestaurantManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("")));
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddSingleton<IEmployee2Repository,MockEmployee2Repository>();
         }
